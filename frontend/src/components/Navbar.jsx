c614dd2bf6894e18f89e4f03f03452490ddcc6b8
CaribHut <thecaribhut@gmail.com>
2026-03-03 14:36:19 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Menu, X } from 'lucide-react';
-import logo from "../assets/ch-logo-circ-vpink.png";
+import logo from "../assets/ch-logo-pink.png";
 
 const Navbar = ({ onBookingClick }) => {
   const [isScrolled, setIsScrolled] = useState(false);
