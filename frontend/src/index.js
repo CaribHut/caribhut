75023ca02fd1660df4f4817514b645ad4f1f37bc
CaribHut <thecaribhut@gmail.com>
2026-03-05 22:53:54 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
+import { Analytics } from "@vercel/analytics/react"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <Analytics />
+  </React.StrictMode>
+),
 );
