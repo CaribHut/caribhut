3a6dbb678304b512f2177c8084f1bea0d940b145
CaribHut <thecaribhut@gmail.com>
2026-03-06 01:41:39 +0100

    Create bookings.js

---
+++ frontend/api/bookings.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  try {
+    const booking = req.body;
+
+    console.log("Ny bokning:", booking);
+
+    return res.status(200).json({
+      success: true,
+      message: "Booking received"
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
+  }
+}
