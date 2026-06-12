2ffdfd90dd3f887652688155bdae4a2b4032f565
CaribHut <thecaribhut@gmail.com>
2026-06-12 21:49:50 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Infocards from "./components/InfoCards";
+import InfoCards from "./components/InfoCards";
 import BrandBanner from "./components/BrandBanner";
 import MenuSection from "./components/MenuSection";
 import NightMenuSection from "./components/NightMenuSection";
